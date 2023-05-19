fa2b93d2f5bd32205592fefec36ebf7f8d671b24
Hifzza <94057666+Hifzza@users.noreply.github.com>
2023-05-19 09:45:53 +0200

    unit testing for scopechecker and symboltable

---
+++ c-plus/src/test/java/org/example/typechecking/ScopeCheckerTest.java
@@ -0,0 +1,12 @@
+package org.example.typechecking; //junit 3
+
+import junit.framework.TestCase;
+
+public class ScopeCheckerTest extends TestCase {
+
+    public void testEnterScope() {
+    }
+
+    public void testExitScope() {
+    }
+}
---
+++ c-plus/src/test/java/org/example/typechecking/SymbolTableTest.java
@@ -0,0 +1,12 @@
+package org.example.typechecking; //junit 3
+
+import junit.framework.TestCase;
+
+public class SymbolTableTest extends TestCase {
+
+    public void testAddSymbol() {
+    }
+
+    public void testLookupSymbol() {
+    }
+}
