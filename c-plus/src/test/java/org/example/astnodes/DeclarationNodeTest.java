9b446db5d32d689071309f77cc287bb47819d3b6
JakobT97 <jakobthoroe@gmail.com>
2023-05-22 19:28:26 +0200

    DclNodeTest

--- c-plus/src/test/java/org/example/astnodes/DeclarationNodeTest.java
+++ c-plus/src/test/java/org/example/astnodes/DeclarationNodeTest.java
@@ -1,5 +1,6 @@
 package org.example.astnodes;
 
+import org.example.AstVisitor;
 import org.example.astnodes.DeclarationNode;
 import org.example.astnodes.TypeSpecifierNode;
 import org.junit.jupiter.api.Assertions;
@@ -8,17 +9,38 @@ import org.junit.jupiter.api.Test;
 class DeclarationNodeTest {
 
     DeclarationNode DclNode = new DeclarationNode();
+    Boolean isConstant = new Boolean(String.valueOf(1));
     TypeSpecifierNode Jakob = new TypeSpecifierNode("int");
+
+    IdentifierNode IdNode = new IdentifierNode("Id123");
+    ExpressionNode value = new ExpressionNode() {
+        @Override
+        public Object accept(AstVisitor visitor) {
+            return super.accept(visitor);
+        }
+    };
 
 
     @Test
-    void typeSpecifierNodeTest() {
-
+    void settingTypeSpecifierNodeTest() {
 
       DclNode.setTypeSpecifierNode(Jakob);
-        Assertions.assertEquals("int", DclNode.getTypeSpecifierNode().getType());
+      Assertions.assertEquals("int", DclNode.getTypeSpecifierNode().getType());
     }
 
-    //more tests
-
+    @Test
+    void settingIdentifierNodeTest(){
+        DclNode.setIdentifierNode(IdNode);
+        Assertions.assertEquals("Id123", DclNode.getIdentifierNode().getName());
+    }
+    @Test
+    void valueTest(){
+        DclNode.setValue(value);
+        Assertions.assertEquals(DclNode.getValue(),value);
+    }
+    @Test
+    void isConstTest(){
+        DclNode.setConst(isConstant);
+        Assertions.assertEquals(isConstant,DclNode.getConst());
+    }
 }
