223d9a5f44624224464bc5af5f17eab8c6155608
JakobT97 <jakobthoroe@gmail.com>
2023-05-22 11:11:59 +0200

    declarationNodeTest

---
+++ c-plus/src/test/java/org/example/typechecking/DeclarationNodeTest.java
@@ -0,0 +1,25 @@
+package org.example.typechecking;
+
+import org.example.astnodes.DeclarationNode;
+import org.example.astnodes.TypeSpecifierNode;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class DeclarationNodeTest {
+
+    DeclarationNode DclNode = new DeclarationNode();
+    TypeSpecifierNode Jakob = new TypeSpecifierNode("int");
+
+
+
+    @Test
+    void typeSpecifierNodeTest() {
+
+
+      DclNode.setTypeSpecifierNode(Jakob);
+        Assertions.assertEquals("int", DclNode.getTypeSpecifierNode().getType());
+    }
+
+    //more tests
+
+}
--- c-plus/src/test/java/org/example/typechecking/ScopeCheckerTest.java
+++ c-plus/src/test/java/org/example/typechecking/ScopeCheckerTest.java
@@ -1,9 +1,15 @@
 package org.example.typechecking; //junit 3
 
 import junit.framework.TestCase;
+import org.junit.jupiter.api.Test;
 
 public class ScopeCheckerTest extends TestCase {
 
+    @Test
+    void openScopeTest(){
+        SymbolTable.dep
+
+    }
     public void testEnterScope() {
     }
 
