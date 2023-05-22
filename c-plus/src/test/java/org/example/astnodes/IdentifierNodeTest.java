ab5a7a43aa29575d59ef1c6321bf026b84891133
JakobT97 <jakobthoroe@gmail.com>
2023-05-22 11:33:05 +0200

    IdNodeTest

--- c-plus/src/test/java/org/example/astnodes/IdentifierNodeTest.java
+++ c-plus/src/test/java/org/example/astnodes/IdentifierNodeTest.java
@@ -1,7 +1,22 @@
 package org.example.astnodes;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class IdentifierNodeTest {
+
+    IdentifierNode IdNode = new IdentifierNode("Jakob");
+    
+
+
+
+    @Test
+    void IdNodeTest() {
+
+        Assertions.assertEquals("Jakob", IdNode.getName());
+
+    }
 
 }
