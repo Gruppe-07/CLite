c1a7b5dae217fbd9d5aa16b3e9ebb1ae2c04bfd5
Fantaskink <45458221+Fantaskink@users.noreply.github.com>
2024-09-20 22:11:11 +0200

    update c grammar

--- c-plus/assembly/output.s
+++ c-plus/assembly/output.s
@@ -3,41 +3,17 @@
 
 _main: STP LR, FP, [SP, #-16]! //Push LR, FP onto stack
    // Make room for local variables and potential parameter
-   SUB FP, SP, #0
-   SUB SP, SP, #0
+   SUB FP, SP, #16
+   SUB SP, SP, #16
 
+   // Declare i
    MOV X1, #1
    MOV X2, #1
-   CMP X1, X2 // X1 > X2 
-   B.LT elseclause
-   // Declare a
-   MOV X0, #1 // Load parameter into X0
-   // Setup
-   STP X29, LR, [SP, #-16]!     ; Save LR, FR
-   ADRP X0, ptfStr@PAGE
-   ADD X0, X0, ptfStr@PAGEOFF
-   MOV X10, #1
-   STR X10, [SP, #-32]!
-   BL _printf
-   MOV X1, #1 // Dummy return value from printf
-   STR X1, [FP, #-0]
+   ADD X3, X1, X2
 
-   B endif
-elseclause: 
-   // Declare a
-   MOV X0, #1 // Load parameter into X0
-   // Setup
-   STP X29, LR, [SP, #-16]!     ; Save LR, FR
-   ADRP X0, ptfStr@PAGE
-   ADD X0, X0, ptfStr@PAGEOFF
-   MOV X10, #1
-   STR X10, [SP, #-32]!
-   BL _printf
-   MOV X1, #1 // Dummy return value from printf
-   STR X1, [FP, #-8]
+   STR X3, [FP, #0]
 
-endif:
-   ADD SP, SP, #0
+   ADD SP, SP, #16
    LDP LR, FP, [SP], #16 // Restore LR, FP
    MOV X0, #0
    MOV X16, #1
